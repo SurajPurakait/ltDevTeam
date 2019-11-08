2879cc3f3cfb10594b615766c03627fe4c4b04ec
sumanta-sysalgo <55100651+sumanta-sysalgo@users.noreply.github.com>
2019-11-08 19:55:13 +0530

    Project change@11/08/2019

--- application/helpers/system_helper.php
+++ application/helpers/system_helper.php
@@ -2628,7 +2628,11 @@ if (!function_exists('getProjectTaskList')) {
     function getProjectTaskList($template_id) {
         $ci = &get_instance();
         $ci->load->model('Project_Template_model');
-        return $ci->db->get_where('project_task', ['project_id' => $template_id])->result();
+        $ci->db->select('pt.*,pm.template_cat_id');
+        $ci->db->from('project_task  pt');
+        $ci->db->join('project_main  pm','pt.project_id=pm.project_id');
+        $ci->db->where('pt.project_id',$template_id);
+        return $ci->db->get()->result();
     }
 
 }
@@ -3044,16 +3048,6 @@ if (!function_exists('get_service_notifications_count')) {
         $ci = &get_instance();
         $ci->load->model('system');
         return $ci->system->get_service_notifications_count($forvalue);
-    }
-
-}
-
-if (!function_exists('get_lead_notifications_count')) {
-
-    function get_lead_notifications_count() {
-        $ci = &get_instance();
-        $ci->load->model('system');
-        return $ci->system->get_lead_notifications_count();
     }
 
 }
