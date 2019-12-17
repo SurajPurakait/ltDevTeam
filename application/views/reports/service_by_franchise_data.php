d17772e47f011f1c257411c41d0dd607ddebccc0
SUVAYANSAM319 <36656502+SUVAYANSAM319@users.noreply.github.com>
2019-12-17 16:04:36 +0530

    report dashboard pie chart

--- application/views/reports/service_by_franchise_data.php
+++ application/views/reports/service_by_franchise_data.php
@@ -1,6 +1,8 @@
 <?php
     if ($category == 'franchise') {    
 ?>
+<div class="row">
+    <div class="col-md-10">
 <table class="table table-bordered report-table table-striped text-center m-b-0">
     <thead>
         <tr>
@@ -13,19 +15,7 @@
             <th>< 60</th>
             <th>+ 60</th>
             <th>SOS</th>
-            <th>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="franchise_office" width="60"></div>
-                        <script type="text/javascript">
-                            
-                        </script>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="franchise_tracking" width="60"></div>
-                    </div>
-                </div>
-            </th>
+            
         </tr>
     </thead>
     <tbody>
@@ -42,24 +32,87 @@
             <td><?= $value['less_than_60']; ?></td>
             <td><?= $value['more_than_60']; ?></td>
             <td><?= $value['sos']; ?></td>
-            <td></td>
         </tr>
         <?php 
             }
         ?>
     </tbody>
 </table>
-<script type="text/javascript">
-    pieChart('franchise_office');
-    pieChart('franchise_tracking');
-</script>
+</div>
+<div class="col-md-2 m-t-40">
+    <h4 class="text-center">Offices</h4>
+    <?php 
+        foreach ($reports as $key => $value) {
+            $data = array_column($service_by_franchise_list,'totals');        
+    ?>
+    <div class="service-franchise-donut-<?= $key ?> text-center" data-size="120" id="service_franchise_donut_<?= $key ?>" data-json="service_franchise_data_<?= $key ?>"></div>
+    
+    <script>
+        var service_franchise_data_<?= $key ?> = [
+            {'section_label': 'Contador Fort Lauderdale ', 'value': <?= $data[0]; ?>, 'color': '#FFB046'}, 
+            {'section_label': 'Contador Miami ', 'value': <?= $data[1]; ?>, 'color': '#06a0d6'}, 
+            {'section_label': 'Contador Orlando', 'value': <?= $data[2]; ?>, 'color': '#ff8c1a'},
+            {'section_label': 'Contador Sunny Isles', 'value':<?= $data[3];?>, 'color': '#009900'},
+            {'section_label': 'Corporate', 'value':<?= $data[4];?>, 'color': '#663300'},
+            {'section_label': 'LeafNet Office', 'value':<?= $data[5];?>, 'color': '#ff66cc'},
+            {'section_label': 'Nalogi Miami', 'value':<?= $data[6];?>, 'color': '#ffdb4d'},
+            {'section_label': 'TaxLeaf Aventura', 'value':<?= $data[7];?>, 'color': '#00ff99'},
+            {'section_label': 'TaxLeaf Coral Gables', 'value':<?= $data[8];?>, 'color': '#99ff99'},
+            {'section_label': 'TaxLeaf Coral Springs', 'value':<?= $data[9];?>, 'color': '#669900'},
+            {'section_label': 'TaxLeaf Doral', 'value':<?= $data[10];?>, 'color': '#ffcc00'},
+            {'section_label': 'Taxleaf DSM', 'value':<?= $data[11];?>, 'color': '#99ff66'},
+            {'section_label': 'TaxLeaf Fort Lauderdale', 'value':<?= $data[12];?>, 'color': '#66ffff'},
+            {'section_label': 'TaxLeaf Hallandale', 'value':<?= $data[13];?>, 'color': '#ff8c66'},
+            {'section_label': 'TaxLeaf Hialeah', 'value':<?= $data[14];?>, 'color': '#e600ac'},
+            {'section_label': 'TaxLeaf Kendall', 'value':<?= $data[15];?>, 'color': '#aaaa55'},
+            {'section_label': 'TaxLeaf Lake Mary', 'value':<?= $data[16];?>, 'color': '#ff9900'},
+            {'section_label': 'TaxLeaf Miramar', 'value':<?= $data[17];?>, 'color': '#004de6'},
+            {'section_label': 'TaxLeaf North Miami Beach', 'value':<?= $data[18];?>, 'color': '#993333'},
+            {'section_label': 'TaxLeaf Pembroke Pines', 'value':<?= $data[19];?>, 'color': '#003300'}
+        ];                    
+    </script>
+    <script>
+         pieChart('service-franchise-donut-<?= $key ?>');
+    </script>
+    <?php
+         }
+    ?>
+
+    <h4 class="m-t-40 text-center">Tracking</h4>
+    <?php 
+        foreach ($reports as $key => $value) {
+            $new = array_sum(array_column($service_by_franchise_list,'new'));
+            $started = array_sum(array_column($service_by_franchise_list,'started')); 
+            $completed = array_sum(array_column($service_by_franchise_list,'completed'));
+            // echo $new."-".$Started."-".$completed;          
+    ?>
+    <div class="service-tracking-donut-<?= $key ?> text-center" data-size="120" id="service_tracking_donut_<?= $key ?>" data-json="service_tracking_data_<?= $key ?>"></div>
+    
+    <script>
+        var service_tracking_data_<?= $key ?> = [
+            {'section_label': 'New ', 'value': <?= $new; ?>, 'color': '#1c84c6'}, 
+            {'section_label': 'Started ', 'value': <?= $started; ?>, 'color': '#f8ac59'}, 
+            {'section_label': 'Completed', 'value': <?= $completed; ?>, 'color': '#1ab394'},
+        ];                    
+    </script>
+    <script>
+         pieChart('service-tracking-donut-<?= $key ?>');
+    </script>
+<?php
+    }
+?>    
+</div>
+</div>
 <?php
     } else if ($category == 'department') { 
 ?>
+
+<div class="row">
+    <div class="col-md-10">
 <table class="table table-bordered report-table table-striped text-center m-b-0">
     <thead>
         <tr>
-            <th>Offices</th>
+            <th>Departments</th>
             <th>Totals</th>
             <th>New</th>
             <th>Started</th>
@@ -68,16 +121,6 @@
             <th>< 60</th>
             <th>+ 60</th>
             <th>SOS</th>
-            <th>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="department_office" width="60"></div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="department_tracking" width="60"></div>
-                    </div>
-                </div>
-            </th>
         </tr>
     </thead>
     <tbody>
@@ -94,24 +137,76 @@
             <td><?= $value['less_than_60']; ?></td>
             <td><?= $value['more_than_60']; ?></td>
             <td><?= $value['sos']; ?></td>
-            <td></td>
         </tr>
         <?php 
             }
         ?>
     </tbody>
 </table>
-<script type="text/javascript">
-    pieChart('department_office');
-    pieChart('department_tracking');
-</script>
+        
+    </div>
+    <div class="col-md-2">
+        <h4 class="m-t-40 text-center">Department</h4>
+        <?php 
+            foreach ($reports as $key => $value) {
+                    $data = array_column($service_by_franchise_list,'totals');        
+            ?>
+            <div class="service-department-donut-<?= $key ?> text-center" data-size="120" id="service_department_donut_<?= $key ?>" data-json="service_department_data_<?= $key ?>"></div>
+            
+            <script>
+                var service_department_data_<?= $key ?> = [
+                    {'section_label': 'Billing ', 'value': <?= $data[0]; ?>, 'color': '#FFB046'}, 
+                    {'section_label': 'Payroll ', 'value': <?= $data[1]; ?>, 'color': '#06a0d6'}, 
+                    {'section_label': 'Bookkeeping', 'value': <?= $data[2]; ?>, 'color': '#ff8c1a'},
+                    {'section_label': 'Data', 'value':<?= $data[3];?>, 'color': '#009900'},
+                    {'section_label': 'Franchisor', 'value':<?= $data[4];?>, 'color': '#663300'},
+                    {'section_label': 'Government', 'value':<?= $data[5];?>, 'color': '#ff66cc'},
+                    {'section_label': 'Marketing', 'value':<?= $data[6];?>, 'color': '#ffdb4d'},
+                    {'section_label': 'Leafnet', 'value':<?= $data[7];?>, 'color': '#00ff99'},
+                    {'section_label': 'Leafcloud', 'value':<?= $data[8];?>, 'color': '#99ff99'},
+                    {'section_label': 'CPA ', 'value':<?= $data[9];?>, 'color': '#669900'},
+                    {'section_label': 'Admin', 'value':<?= $data[10];?>, 'color': '#ffcc00'},
+                ];                    
+            </script>
+            <script>
+                 pieChart('service-department-donut-<?= $key ?>');
+            </script>
+            <h4 class="m-t-40 text-center">Tracking</h4>
+            <?php
+                 }
+            ?>
+            <?php 
+                foreach ($reports as $key => $value) {
+                    $new = array_sum(array_column($service_by_franchise_list,'new'));
+                    $started = array_sum(array_column($service_by_franchise_list,'started')); 
+                    $completed = array_sum(array_column($service_by_franchise_list,'completed'));          
+            ?>
+            <div class="department-tracking-donut-<?= $key ?> text-center" data-size="120" id="department_tracking_donut_<?= $key ?>" data-json="department_tracking_data_<?= $key ?>"></div>
+            
+            <script>
+                var department_tracking_data_<?= $key ?> = [
+                    {'section_label': 'New ', 'value': <?= $new; ?>, 'color': '#1c84c6'}, 
+                    {'section_label': 'Started ', 'value': <?= $started; ?>, 'color': '#f8ac59'}, 
+                    {'section_label': 'Completed', 'value': <?= $completed; ?>, 'color': '#1ab394'},
+                ];                    
+            </script>
+            <script>
+                 pieChart('department-tracking-donut-<?= $key ?>');
+            </script>
+            <?php
+                 }
+            ?>
+    </div>
+</div>
 <?php 
     } else if ($category == 'service_category') {
 ?>
+<div class="row">
+    <div class="col-md-10">
 <table class="table table-bordered report-table table-striped text-center m-b-0">
     <thead>
         <tr>
-            <th>Offices</th>
+            <th>Category</th>
             <th>Totals</th>
             <th>New</th>
             <th>Started</th>
@@ -120,16 +215,6 @@
             <th>< 60</th>
             <th>+ 60</th>
             <th>SOS</th>
-            <th>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="category_office" width="60"></div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="category_tracking" width="60"></div>
-                    </div>
-                </div>
-            </th>
         </tr>
     </thead>
     <tbody>
@@ -146,17 +231,60 @@
             <td><?= $value['less_than_60']; ?></td>
             <td><?= $value['more_than_60']; ?></td>
             <td><?= $value['sos']; ?></td>
-            <td></td>
         </tr>
         <?php 
             }
         ?>
     </tbody>
 </table>
-<script type="text/javascript">
-    pieChart('category_office');
-    pieChart('category_tracking');    
-</script>
+    </div>
+    <div class="col-md-2">
+            <h4 class="text-center">Category</h4>
+            <?php 
+                foreach ($reports as $key => $value) {
+                    $data = array_column($service_by_franchise_list,'totals');        
+            ?>
+            <div class="service-category-donut-<?= $key ?> text-center" data-size="120" id="service_category_donut_<?= $key ?>" data-json="service_category_data_<?= $key ?>"></div>
+            
+            <script>
+                var service_category_data_<?= $key ?> = [
+                    {'section_label': 'Incorporation ', 'value': <?= $data[0]; ?>, 'color': '#FFB046'}, 
+                    {'section_label': 'Accounting Services ', 'value': <?= $data[1]; ?>, 'color': '#06a0d6'}, 
+                    {'section_label': 'Tax Services', 'value': <?= $data[2]; ?>, 'color': '#ff8c1a'},
+                    {'section_label': 'Business Services', 'value':<?= $data[3]; ?>, 'color': '#009900'},
+                    {'section_label': 'Partner Services', 'value':<?= $data[4]; ?>, 'color': '#663300'},
+                ];                    
+            </script>
+            <script>
+                 pieChart('service-category-donut-<?= $key ?>');
+            </script>
+            <?php
+                 }
+            ?>
+            <h4 class="text-center">Tracking</h4>
+            <?php 
+                foreach ($reports as $key => $value) {
+                    $new = array_sum(array_column($service_by_franchise_list,'new'));
+                    $started = array_sum(array_column($service_by_franchise_list,'started')); 
+                    $completed = array_sum(array_column($service_by_franchise_list,'completed'));          
+            ?>
+            <div class="category-tracking-donut-<?= $key ?> text-center" data-size="120" id="category_tracking_donut_<?= $key ?>" data-json="category_tracking_data_<?= $key ?>"></div>
+            
+            <script>
+                var category_tracking_data_<?= $key ?> = [
+                    {'section_label': 'New ', 'value': <?= $new; ?>, 'color': '#1c84c6'}, 
+                    {'section_label': 'Started ', 'value': <?= $started; ?>, 'color': '#f8ac59'}, 
+                    {'section_label': 'Completed', 'value': <?= $completed; ?>, 'color': '#1ab394'},
+                ];                    
+            </script>
+            <script>
+                 pieChart('category-tracking-donut-<?= $key ?>');
+            </script>
+            <?php
+                 }
+            ?>
+    </div>
+</div>
 <?php
     }
 ?>
