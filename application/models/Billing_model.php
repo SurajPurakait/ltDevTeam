ae047619dbd7ad3d48b43fd106fbbdd8fbdc938e
SUVAYANSAM319 <36656502+SUVAYANSAM319@users.noreply.github.com>
2020-01-28 20:16:28 +0530

    report refresh

--- application/models/Billing_model.php
+++ application/models/Billing_model.php
@@ -3053,6 +3053,12 @@ class Billing_model extends CI_Model {
             return $z;
         }
     }
+
+    public function recurring($invoice_id){
+      return $this->db->query("select is_recurrence from invoice_info where id = $invoice_id")->result_array(); 
+      
+    }
+
     public function refresh_report_dashboard_billing() {
         $select = [
             'inv.id as invoice_id',
@@ -3148,8 +3154,228 @@ class Billing_model extends CI_Model {
         return 1;
     }
 
-    public function recurring($invoice_id){
-      return $this->db->query("select is_recurrence from invoice_info where id = $invoice_id")->result_array(); 
-      
+    public function refresh_royalty_report_data() {
+        $servername = "localhost";
+        $username = "leafnet_db_user";
+        $password = "leafnet@123";
+        $db = 'leafnet_stagings';
+
+        // $servername = "localhost";
+        // $username = "root";
+        // $password = "";
+        // $db = 'leafnet';
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $db);
+
+        // Check connection
+        if($conn === false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+        }
+        $select = [
+            'inv.id as invoice_id',
+            'inv.reference_id as reference_id',
+            'inv.order_id as order_id',
+            'inv.new_existing as new_existing',
+            'inv.created_time as created_time',
+            'srv.price_charged as override_price',
+            'srv.status as service_status',
+            'srv.services_id as service_id',
+            'inv.existing_reference_id as existing_reference_id',
+            'inv.type as invoice_type',
+            'inv.is_order as is_order',
+            'inv.created_by AS created_by',
+            'inv.payment_status AS payment_status',
+            'inv.total_amount AS sub_total',
+            'inv.client_id as client_id',
+            'indt.practice_id as practice_id',
+            '(CASE WHEN inv.type = 1 THEN (SELECT `company`.`name` FROM `company` WHERE `company`.`id` = `inv`.`client_id`) ELSE (SELECT CONCAT(individual.last_name,", ",individual.first_name) FROM `individual` WHERE `individual`.`id` = `inv`.`client_id`) END) as client_name',
+            'inv.start_month_year as start_month_year',
+            'inv.existing_practice_id as existing_practice_ID',
+            'inv.status as invoice_status',
+            '(SELECT COUNT(*) FROM `order` WHERE invoice_id = inv.id AND reference = \'invoice\') as services',
+            'indt.office as office_id',
+            '(SELECT ofc.name FROM office as ofc WHERE ofc.id = indt.office) as office',
+            '(SELECT ofc.office_id FROM office as ofc WHERE ofc.id = indt.office) as officeid',
+            '(SELECT concat(st.last_name, ", ", st.first_name) FROM staff as st WHERE st.id = indt.partner) as partner',
+            '(SELECT concat(st.last_name, ", ", st.first_name) FROM staff as st WHERE st.id = indt.manager) as manager',
+            '(SELECT concat(st.last_name, ", ", st.first_name) FROM staff as st WHERE st.id = inv.created_by) as created_by_name',
+            '(SELECT CONCAT(",",GROUP_CONCAT(`service_id`), ",") FROM `order` WHERE `invoice_id` = inv.id AND `reference` = "invoice") AS all_services',
+            '(SELECT CONCAT(",",GROUP_CONCAT(`id`), ",") FROM `order` WHERE `invoice_id` = inv.id AND `reference` = "invoice") AS all_orders',
+            '(SELECT CONCAT(",",GROUP_CONCAT(`total_of_order`), ",") FROM `order` WHERE `invoice_id` = inv.id AND `reference` = "invoice") AS all_services_override',
+            '(SELECT CONCAT(",",GROUP_CONCAT(`payment_type`), ",") FROM `payment_history` WHERE `invoice_id` = ord.invoice_id AND `order_id` = ord.id) AS payment_types',
+            '(SELECT SUM(pay_amount) FROM payment_history WHERE payment_history.type = \'payment\' AND payment_history.invoice_id = inv.id AND payment_history.is_cancel = 0) AS pay_amount',
+        ];
+        $where['ord.reference'] = '`ord`.`reference` = \'invoice\' ';
+        $where['status'] = 'AND `inv`.`status` != 0 ';
+
+        $table = '`invoice_info` AS `inv` ' .
+            'INNER JOIN `order` AS `ord` ON `ord`.`invoice_id` = `inv`.`id` ' .
+            'INNER JOIN `internal_data` AS `indt` ON (CASE WHEN `inv`.`type` = 1 THEN `indt`.`reference_id` = `inv`.`client_id` AND `indt`.`reference` = "company" ELSE `indt`.`reference_id` = `inv`.`client_id` AND `indt`.`reference` = "individual" END)'.
+            'INNER JOIN `service_request` AS `srv` ON `srv`.`order_id` = `inv`.`order_id`'. 
+            'LEFT JOIN `payment_history` AS `pyh` ON `pyh`.`invoice_id` = `inv`.`id`';
+
+        $query = 'SELECT ' . implode(', ', $select) . ' FROM ' . $table . 'WHERE ' . implode('', $where) . ' GROUP BY `ord`.`invoice_id`';
+
+        mysqli_query($conn, 'SET SQL_BIG_SELECTS=1');
+        $reports_data = mysqli_query($conn,$query);
+        $reports_data_count = mysqli_num_rows($reports_data);
+
+        if ($reports_data_count > 0) {
+            while ($rpd = mysqli_fetch_assoc($reports_data)) { 
+                for($i=1; $i <= $rpd['services']; $i++) {
+                    $services_id = explode(',',$rpd['all_services'])[$i];
+                    $services_query = "select s.id, s.category_id, s.fixed_cost as cost, c.name as category_name, c.description as category_description, s.description, s.ideas, s.tutorials, s.dept AS service_department, s.retail_price from services s inner join category c on c.id = s.category_id where s.id ='".$services_id."'";
+                    $services_query_run = mysqli_query($conn,$services_query);
+                    $service_detail = mysqli_fetch_assoc($services_query_run);
+
+                    $sql_p = "select percentage from office_service_fees where service_id = '".$services_id."' and office_id = '".$rpd['office_id']."'";
+                    $office_fees_qry = mysqli_query($conn,$sql_p);
+                    $office_fees_result=mysqli_fetch_assoc($office_fees_qry);
+                    $office_fees=$office_fees_result['percentage'];
+                    $order_id = explode(',',$rpd['all_orders'])[$i];
+                    $p_h_sql = "select pay.reference_no AS reference,pay.authorization_id,typ.name AS payment_type,pay.pay_amount AS collected from payment_history pay inner join payment_type typ on typ.id = pay.payment_type where type = 'payment' and is_cancel != '1' and invoice_id = '".$rpd['invoice_id']."' and order_id = '".$order_id."'";
+                    $pay_history = mysqli_query($conn,$p_h_sql);
+                    $payment_history = [];
+                    while ($payment = mysqli_fetch_assoc($pay_history)) {
+                        array_push($payment_history,$payment);
+                    } 
+                    $reference = implode(',',array_column($payment_history,'reference'));
+                    $authorization_id_without_slash = implode(',',array_column($payment_history,'authorization_id'));
+                    $authorization_id = addslashes($authorization_id_without_slash);
+                    $payment_type = implode(',',array_column($payment_history,'payment_type'));
+                    $collected = array_sum(array_column($payment_history,'collected')); 
+                    $override_price = explode(',',$rpd['all_services_override'])[$i];
+                    $total_net = ($override_price != '') ? (int)$override_price - (int)$service_detail['cost'] : (int)$service_detail['retail_price'] - (int)$service_detail['cost'];
+                    $date_val = date('Y-m-d', strtotime($rpd['created_time']));
+                    $fee_with_cost = (((int)$total_net * (int)$office_fees)/100); 
+                    $fee_without_cost = (((int)$override_price * (int)$office_fees)/100);
+                    if (($override_price - $collected) == 0) {
+                        $payment_status = 'Paid';
+                    } elseif (($override_price - $collected) == $override_price) {
+                        $payment_status = 'Unpaid';
+                    } elseif (($override_price - $collected) > 0 && ($override_price - $collected) < $override_price) {
+                        $payment_status = 'Partial';
+                    } else {
+                        $payment_status = 'Late';
+                    }
+                    $practice_id = $rpd['practice_id'];
+                    $invoice_id = $rpd['invoice_id'];
+                    $services_ids = $rpd['invoice_id'].'-'.$i;
+                    
+                    if (!empty($service_detail['description'])) {
+                        $service_details = $service_detail['description'];
+                    } else {
+                        $service_details = "N/A";
+                    }
+                    $retail_price = $service_detail['retail_price'];
+                    $service_cost = $service_detail['cost'];
+                    $office_id = $rpd['office_id'];
+                    $office_id_name = $rpd['officeid'];
+                    $created_by = $rpd['created_by'];
+                    
+                    $comparison_array = array(
+                        'date' => $date_val,
+                        'client_id' => $practice_id,
+                        'invoice_id' => $invoice_id,
+                        'service_id' => $services_ids,
+                        'service_name' => $service_details,
+                        'retail_price' => $retail_price,
+                        'override_price' => $override_price,
+                        'cost' => $service_cost,
+                        'payment_status' => $payment_status,
+                        'collected' => $collected,
+                        'payment_type' => $payment_type,
+                        'authorization_id' => $authorization_id,
+                        'reference' => $reference,
+                        'total_net' => $total_net,
+                        'office_fee' => $office_fees,
+                        'fee_with_cost' => $fee_with_cost,
+                        'fee_without_cost' => $fee_without_cost,
+                        'office_id' => $office_id,
+                        'office_id_name' => $office_id_name,
+                        'created_by' => $created_by
+                    );
+                    
+                    $invoice_id_inner = $invoice_id."-".$i;
+
+                    // fetching data from royalty report table
+                    $royalty_sql = "SELECT * FROM `royalty_report` WHERE service_id = '".$invoice_id_inner."'";
+                    $royalty_query_run = mysqli_query($conn,$royalty_sql);
+                    $rqr = mysqli_fetch_assoc($royalty_query_run);
+                        if ($invoice_id_inner == $rqr['service_id']) {
+                            unset($rqr['id']);
+                            if (empty(array_diff($comparison_array,$rqr))) {
+                                echo "No Difference with previous values";
+                            } else {
+                                $update_sql = "UPDATE `royalty_report` SET ";
+                                if ($date_val != $rqr['date']) {
+                                    $update_sql .= "`date`='$date_val',";
+                                } 
+                                if ($practice_id != $rqr['client_id']) {
+                                    $update_sql .= "`client_id`='$practice_id',";
+                                }
+                                if ($service_details != $rqr['service_name']) {
+                                    $update_sql .= "`service_name`='$service_details',";
+                                }
+                                if ($retail_price != $rqr['retail_price']) {
+                                    $update_sql .= "`retail_price`='$retail_price',";
+                                }
+                                if ($override_price != $rqr['override_price']) {
+                                    $update_sql .= "`override_price` = '$override_price',";
+                                }
+                                if ($payment_status != $rqr['payment_status']) {
+                                    $update_sql .= "`payment_status`= '$payment_status',";
+                                }
+                                if ($collected != $rqr['collected']) {
+                                    $update_sql .= "`collected`='$collected',"; 
+                                }
+                                if ($payment_type != $rqr['payment_type']) {
+                                    $update_sql .= "`payment_type`='$payment_type',";
+                                }
+                                if ($authorization_id_without_slash != $rqr['authorization_id']) {
+                                    $update_sql .= "`authorization_id`='$authorization_id',";
+                                }
+                                if ($reference != $rqr['reference']) {
+                                    $update_sql .= "`reference`='$reference',";
+                                }
+                                if ($total_net != $rqr['total_net']) {
+                                    $total_net_modified = (int)$override_price - (int)$rqr['cost'];
+                                    $update_sql .= "`total_net`='$total_net_modified',";
+                                }
+                                if ($fee_with_cost != $rqr['fee_with_cost']) {
+                                    $fee_with_cost_modified = ((int)$override_price - (int)$rqr['cost'] ) * ((int)$rqr['office_fee'] / 100);
+                                    $update_sql .= "`fee_with_cost`='$fee_with_cost_modified',"; 
+                                }
+                                if ($fee_without_cost != $rqr['fee_without_cost']) {
+                                    $fee_without_cost_modified = (int)$override_price * ((int)$rqr['office_fee'] / 100);
+                                    $update_sql .= "`fee_without_cost`= '$fee_without_cost_modified',";
+                                }
+                                if($office_id_name != $rqr['office_id_name']) {
+                                    $update_sql .= "`office_id_name`='$office_id_name',";
+                                }
+                                if (substr($update_sql, -1) == ',') {
+                                    $update_sql = substr($update_sql,0,-1);
+                                } 
+                                $update_sql .= " WHERE invoice_id = '".$invoice_id."'";
+                                mysqli_query($conn,$update_sql);
+                                echo $update_sql;
+                            }    
+                        } else {                          
+                            $sql_query = "INSERT INTO `royalty_report`(`date`, `client_id`, `invoice_id`, `service_id`, `service_name`, `retail_price`, `override_price`, `cost`, `payment_status`, `collected`, `payment_type`, `authorization_id`, `reference`, `total_net`, `office_fee`, `fee_with_cost`, `fee_without_cost`, `office_id`,`office_id_name` ,`created_by`) VALUES (
+                            '$date_val', '$practice_id','$invoice_id',
+                            '$services_ids','$service_details','$retail_price',
+                            '$override_price','$service_cost','$payment_status',
+                            '$collected','$payment_type','$authorization_id',
+                            '$reference','$total_net','$office_fees',
+                            '$fee_with_cost','$fee_without_cost','$office_id','$office_id_name',
+                            '$created_by')";
+                            mysqli_query($conn,$sql_query)or die('insert error');
+                            echo $sql_query;
+                        }   
+                        echo "<hr>";    
+                }
+            } 
+            echo '1';
+        }
     }
 }
--- application/models/Service_model.php
+++ application/models/Service_model.php
@@ -3171,4 +3171,196 @@ class Service_model extends CI_Model {
         }
         return 1;
     }
+
+    public function refresh_sales_report_data() {
+        $servername = "localhost";
+        $username = "leafnet_db_user";
+        $password = "leafnet@123";
+        $db = 'leafnet_stagings';
+        
+        // $servername = "localhost";
+        // $username = "root";
+        // $password = "";
+        // $db = 'leafnet';
+
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $db);
+
+        // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. " . mysqli_connect_error());
+        }
+
+        $select = [
+            'inv.id as invoice_id',
+            'ord.id as order_id',
+            'ord.order_date as order_date',
+            'indt.practice_id as client_id',
+            'indt.office as office_id',
+            'srv.responsible_staff as created_by',
+            '(SELECT CONCAT(",",GROUP_CONCAT(`service_id`), ",") FROM `order` WHERE `invoice_id` = inv.id AND `reference` = "invoice") AS all_services',
+            '(SELECT CONCAT(",",GROUP_CONCAT(`total_of_order`), ",") FROM `order` WHERE `invoice_id` = inv.id AND `reference` = "invoice") AS all_override_price',
+            '(SELECT COUNT(*) FROM `order` WHERE invoice_id = inv.id AND reference = \'invoice\') as services_count'
+        ];
+        
+        $where['ord.reference'] = '`ord`.`reference` = \'invoice\' ';
+        $where['inv.status'] = 'AND `inv`.`status` != 0 ';
+
+        $table = '`invoice_info` AS `inv` ' .
+                'INNER JOIN `order` AS `ord` ON `ord`.`invoice_id` = `inv`.`id` ' .
+                'INNER JOIN `internal_data` AS `indt` ON (CASE WHEN `inv`.`type` = 1 THEN `indt`.`reference_id` = `inv`.`client_id` AND `indt`.`reference` = "company" ELSE `indt`.`reference_id` = `inv`.`client_id` AND `indt`.`reference` = "individual" END)'.
+                'INNER JOIN `service_request` AS `srv` ON `srv`.`order_id` = `inv`.`order_id`'; 
+        $query = 'SELECT ' . implode(', ', $select) . ' FROM ' . $table . 'WHERE ' . implode('', $where)  . ' GROUP BY `ord`.`invoice_id`';
+
+        mysqli_query($conn, 'SET SQL_BIG_SELECTS=1');
+        $reports_data = mysqli_query($conn,$query);
+        $reports_data_count = mysqli_num_rows($reports_data);
+
+        if ($reports_data_count > 0) {
+            while($srd = mysqli_fetch_assoc($reports_data)) {
+                for ($i=1; $i <= $srd['services_count'] ; $i++) {
+                    $services_id = explode(',',$srd['all_services'])[$i];
+                    /* get service detail */
+                    $service_query = mysqli_query($conn,"select fixed_cost as cost ,retail_price, description as service_name from services where id ='".$services_id."'");
+                    $service_detail = mysqli_fetch_assoc($service_query);
+                    /* get notes */
+                    $note_sql = "select CONCAT(',',GROUP_CONCAT(`note`),',') as all_notes from notes where reference = 'service' and reference_id ='".$services_id."'";
+                    $notes_query = mysqli_query($conn,$note_sql);
+                    $notes = mysqli_fetch_assoc($notes_query);
+                    /* finalize variables */
+                    if (date('Y-m-d', strtotime($srd['order_date'])) == '1970-01-01') {
+                        $date = 'N/A';
+                    } else {
+                        $date = date('Y-m-d', strtotime($srd['order_date']));
+                    }
+                    $client_id = addslashes($srd['client_id']);
+                    $service_id = $srd['invoice_id']."-".$i;
+                    
+                    $service_name = $service_detail['service_name'];
+                    $retail_price = $service_detail['retail_price'];
+                    $override_price = explode(',',$srd['all_override_price'])[$i];
+                    $cost = $service_detail['cost'];
+
+                    $total_net = (int)$override_price - (int)$cost;
+                    $franchisee_fee = ((int)$override_price * 20) / 100;
+                    $gross_profit = (int)$total_net - (int)$franchisee_fee;
+                    
+                    if ($notes['all_notes'] != '') {
+                        $notes = ltrim($notes['all_notes'],',');
+                    } else{
+                        $notes = 'N/A';
+                    }
+                    $office_id = $srd['office_id'];
+                    $created_by = $srd['created_by'];
+                    
+                     
+                    $s_id = explode(',',$srd['all_services'])[$i];                
+                    $collect_query = 'select SUM(pay_amount) as pay_amount from `payment_history` where type = "payment" and is_cancel = "0" and order_id = "'.$srd['order_id'].'" and service_id = "'.$s_id.'"';
+                    $collected_val = mysqli_query($conn,$collect_query);
+                    $collected = mysqli_fetch_assoc($collected_val)['pay_amount'];
+                    $status_query = 'select status from service_request where order_id = "'.$srd['order_id'].'" and services_id = "'.$s_id.'"';
+                    $status_val = mysqli_query($conn,$status_query);
+                    $status = mysqli_fetch_assoc($status_val)['status'];
+                    if ($status == 0) {
+                        $status = 'Completed';
+                    } elseif ($status == 1) {
+                        $status = 'Started';
+                    } elseif ($status == 2) {
+                        $status = 'Not Started';
+                    } elseif($status == 3) {
+                        $status = 'Late';
+                    }
+
+                    $comparison_array = array(
+                        'date' => $date,
+                        'client_id' => $client_id,
+                        'service_id' => $service_id,
+                        'service_name' => $service_name,
+                        'status' => $status,
+                        'retail_price' => $retail_price,
+                        'override_price' => $override_price,
+                        'cost' => $cost,
+                        'collected' => $collected,
+                        'total_net' => $total_net,
+                        'franchisee_fee' => $franchisee_fee,
+                        'gross_profit' => $gross_profit,
+                        'notes' => $notes,
+                        'office_id' => $office_id,
+                        'created_by' => $created_by
+                    );
+
+                    // fetching data from weekly sales report table
+                    $weekly_sales_sql = "SELECT * FROM `weekly_sales_report` WHERE service_id = '".$service_id."'";
+                    $weekly_sales_query_run = mysqli_query($conn,$weekly_sales_sql);
+                    $wsr = mysqli_fetch_assoc($weekly_sales_query_run);
+
+                    if ($service_id == $wsr['service_id']) {
+                        unset($wsr['id']);
+                        if (empty(array_diff($comparison_array,$wsr))) {
+                            echo "No Difference with previous values";
+                            echo "<hr>";
+                        } else {
+                            $update_sql = "UPDATE `weekly_sales_report` SET ";
+                            if ($date != $wsr['date']) {
+                                $update_sql .= "`date`='$date',";
+                            } 
+                            if ($client_id != $wsr['client_id']) {
+                                $update_sql .= "`client_id`='$client_id',";
+                            }
+                            if ($service_name != $wsr['service_name']) {
+                                $update_sql .= "`service_name`='$service_name',";
+                            }
+                            if ($status != $wsr['status']) {
+                                $update_sql .= "`status`= '$status',";
+                            }
+                            if ($retail_price != $wsr['retail_price']) {
+                                $update_sql .= "`retail_price`='$retail_price',";
+                            }
+                            if ($override_price != $wsr['override_price']) {
+                                $update_sql .= "`override_price` = '$override_price',";
+                            }
+                            if ($cost != $wsr['cost']) {
+                                $update_sql .= "`cost`='$cost',"; 
+                            }
+                            if ($collected != $wsr['collected']) {
+                                $update_sql .= "`collected`='$collected',"; 
+                            }
+                            if ($total_net != $wsr['total_net']) {
+                                $total_net_modified = (int)$override_price - (int)$wsr['cost']; 
+                                // this logic is done because service cost would not be change for already purchased service  
+                                $update_sql .= "`total_net`='$total_net_modified',";
+                            }
+
+                            if ($franchisee_fee != $wsr['franchisee_fee']) {
+                                $update_sql .= "`franchisee_fee`='$franchisee_fee',";
+                            }
+                            if ($gross_profit != $wsr['gross_profit']) {
+                                $update_sql .= "`gross_profit`='$gross_profit',";
+                            }
+                            if ($notes != $wsr['notes']) {
+                                $update_sql .= "`notes`='$notes',";
+                            }
+                            if($office_id != $wsr['office_id']) {
+                                $update_sql .= "`office_id`='$office_id',";
+                            }
+                            if (substr($update_sql, -1) == ',') {
+                                $update_sql = substr($update_sql,0,-1);
+                            } 
+                            $update_sql .= " WHERE service_id = '".$service_id."'";
+                            mysqli_query($conn,$update_sql);
+                            echo $update_sql;
+                            echo "<hr>";
+                        }
+                    } else {    
+                        $sql_query = "INSERT INTO `weekly_sales_report`(`date`, `client_id`, `service_id`, `service_name`, `status`, `retail_price`, `override_price`, `cost`, `collected`, `total_net`, `franchisee_fee`, `gross_profit`, `notes`,`office_id`,`created_by`) VALUES (
+                            '" . $date . "','" . $client_id . "','".$service_id."','".$service_name."','".$status."','".$retail_price."','".$override_price."','".$cost."','".$collected."','".$total_net."','".$franchisee_fee."','".$gross_profit."','".$notes."','".$office_id."','".$created_by."')";
+                        echo $sql_query;
+                        mysqli_query($conn,$sql_query)or die('insert error');
+                        echo "<hr>";
+                    }
+                }
+            }
+        }
+        echo '1';
+    }
 }
