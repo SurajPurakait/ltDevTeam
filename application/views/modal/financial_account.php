390209b4f6e7e1cfd178e7777af7662b7adee82d
Sanjita <sanjita.sysalgo@gmail.com>
2020-02-25 18:58:57 +0530

    Service new dashboard data fetching

--- application/views/modal/financial_account.php
+++ application/views/modal/financial_account.php
@@ -105,7 +105,7 @@ if ($modal_type != "edit"):
                 </div>
                 <div class="modal-footer">
                     <input type="hidden" name="order_id" id="order_id" value="<?= $order_id; ?>">
-                    <input type="hidden" name="company_id" id="company_id" value="<?= $reference_id1; ?>">
+                    <input type="hidden" name="company_id" id="company_id" value="<?= $reference_id; ?>">
                     <input type="hidden" name="edit_id" value="">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                     <button type="button" class="btn btn-primary" id="sub_btn" onclick="save_account('');">Save changes</button>
