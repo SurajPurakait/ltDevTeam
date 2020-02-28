96252635b46ae133ca8832dd7c00f4bac3112ff4
SuvayanSys0101 <55140286+SuvayanSys0101@users.noreply.github.com>
2020-02-28 20:17:02 +0530

    recurring billing

--- application/controllers/billing/Invoice.php
+++ application/controllers/billing/Invoice.php
@@ -296,7 +296,7 @@ class Invoice extends CI_Controller {
                 $type='';
             }
             $render_data['place'] = '<div class="text-center">
-            <button class="btn btn-default m-t-10 m-r-5" type="button" onclick="window.location.href = \'' . base_url('billing/home/index/'.$type) . '\';">Close</button>
+            <button class="btn btn-default m-t-10 m-r-5" type="button" onclick="window.location.href = \'' . base_url('billing/home/index/') . '\';">Close</button>
             <button class="btn btn-success m-t-10 m-r-5" type="button" onclick="placeOrder(\'' . $invoice_id . '\', \'' . implode(',', $render_data['contact_email_list']) . '\');"><i class="fa fa-envelope-o"></i> Email</button>
             <button class="btn bg-purple m-t-10 m-r-5" type="button" onclick="printOrder();"><i class="fa fa-print"></i> Print</button>
             <button class="btn btn-warning m-t-10 m-r-5" type="button" onclick="go(\'billing/invoice/export/' . $invoice_id . '\');"><i class="fa fa-file-pdf-o"></i> Download PDF</button>
